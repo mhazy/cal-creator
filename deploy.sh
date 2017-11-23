26baeef7cb7509c4388dd0fe00b2404b2382c53c
Mark Hayes <mark.hayes@rangle.io>
2017-11-22 23:24:06 -0500

    no cache index.html

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 yarn build
 aws s3 rm s3://${CE_DEPLOY_BUCKET}/dist --recursive
 aws s3 sync ./build s3://${CE_DEPLOY_BUCKET}/dist --acl public-read --cache-control "public, max-age=86400"
-aws s3api copy-object --copy-source ${CE_DEPLOY_BUCKET}/dist/index.html --cache-control "public, max-age=60" --content-type "text/html" --bucket ${CE_DEPLOY_BUCKET} --key dist/index.html --metadata-directive="REPLACE"
+aws s3api copy-object --copy-source ${CE_DEPLOY_BUCKET}/dist/index.html --cache-control "public, no-cache" --content-type "text/html" --bucket ${CE_DEPLOY_BUCKET} --key dist/index.html --metadata-directive="REPLACE"
 
